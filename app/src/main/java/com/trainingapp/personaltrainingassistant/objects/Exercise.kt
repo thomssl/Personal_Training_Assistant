b330f74497071b9a0d8f4143373d1f7af029a0a6
thomssl <steventhomson72@yahoo.ca>
2020-01-06 16:43:19 -0500

    Documented Exercise object

--- app/src/main/java/com/trainingapp/personaltrainingassistant/objects/Exercise.kt
+++ app/src/main/java/com/trainingapp/personaltrainingassistant/objects/Exercise.kt
@@ -3,9 +3,19 @@ package com.trainingapp.personaltrainingassistant.objects
 import com.trainingapp.personaltrainingassistant.enumerators.ExerciseType
 import java.lang.StringBuilder
 
-
+/**
+ * Object to hold gathered data about an exercise (abstract exercise definition, not sets, reps or resistance)
+ * @param id Unique key given to the exercise when a new exercise is inserted. Used to differentiate exercises within sessions
+ * @param name Unique string of text given as the exercise's name. User submitted but must be unique to differentiate exercises. User can use any form of differentiation they desire
+ * @param type ExerciseType enum value given to the exercise. Used to define what the primary and secondary movers are (Muscles or Joints)
+ * @param primaryMover MuscleJoint object containing the name and id to the primary mover
+ * @param secondaryMovers List of the MuscleJoint objects that contain the names and ids of the secondary movers
+ */
 class Exercise(val id: Int, var name: String, var type: ExerciseType, var primaryMover: MuscleJoint, private val secondaryMovers: ArrayList<MuscleJoint>) {
 
+    /**
+     * Method to get the names of all the secondary movers as a joined string
+     */
     fun getSecondaryMoversNames(): String{
         val builder = StringBuilder()
         if (secondaryMovers.size > 0) {
@@ -15,7 +25,10 @@ class Exercise(val id: Int, var name: String, var type: ExerciseType, var primar
         return builder.toString()
     }
 
-    private fun getStrSecondaryMovers(): String{
+    /**
+     * Private method to get the ids of all the secondary movers as a joined string. Used for database operations
+     */
+    private fun getDatabaseSecondaryMovers(): String{
         val builder = StringBuilder()
         if (secondaryMovers.size > 0) {
             secondaryMovers.forEach { builder.append(it.id);builder.append(',') }
@@ -24,21 +37,18 @@ class Exercise(val id: Int, var name: String, var type: ExerciseType, var primar
         return builder.toString()
     }
 
+    /**
+     * Method to get a copy of the secondary movers list
+     */
     fun getLstSecondaryMovers(): ArrayList<MuscleJoint> = ArrayList(secondaryMovers.toList())
 
-    fun addSecondaryMover(muscleJoint: MuscleJoint){
-        secondaryMovers.add(muscleJoint)
-    }
+    //secondary movers operations
+    fun addSecondaryMover(muscleJoint: MuscleJoint) = secondaryMovers.add(muscleJoint)
+    fun removeSecondaryMover(muscleJoint: MuscleJoint) = secondaryMovers.remove(muscleJoint)
+    fun clearSecondaryMovers() = secondaryMovers.clear()
 
-    fun removeSecondaryMover(muscleJoint: MuscleJoint){
-        secondaryMovers.remove(muscleJoint)
-    }
-
-    fun clearSecondaryMovers(){
-        secondaryMovers.clear()
-    }
-
-    fun getInsertCommand(): String = "Insert Into Exercises(exercise_name, exercise_type, primary_mover, secondary_movers) Values('$name', ${type.num}, ${primaryMover.id}, '${getStrSecondaryMovers()}')"
-    fun getUpdateCommand(): String = "Update Exercises Set exercise_name = '$name', exercise_type = ${type.num}, primary_mover = ${primaryMover.id}, secondary_movers = '${getStrSecondaryMovers()}' Where exercise_id = $id"
+    //database operations
+    fun getInsertCommand(): String = "Insert Into Exercises(exercise_name, exercise_type, primary_mover, secondary_movers) Values('$name', ${type.num}, ${primaryMover.id}, '${getDatabaseSecondaryMovers()}')"
+    fun getUpdateCommand(): String = "Update Exercises Set exercise_name = '$name', exercise_type = ${type.num}, primary_mover = ${primaryMover.id}, secondary_movers = '${getDatabaseSecondaryMovers()}' Where exercise_id = $id"
     fun getDeleteCommand(): String = "Delete From Exercises Where exercise_id = $id"
 }
