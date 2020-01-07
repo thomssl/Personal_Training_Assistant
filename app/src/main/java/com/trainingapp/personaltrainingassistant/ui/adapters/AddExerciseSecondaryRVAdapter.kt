5d4b9c02d22739bac20944d0feb77f99a3c7df04
thomssl <steventhomson72@yahoo.ca>
2020-01-07 09:25:36 -0500

    Documented AddExerciseSecondaryRVAdapter

--- app/src/main/java/com/trainingapp/personaltrainingassistant/ui/adapters/AddExerciseSecondaryRVAdapter.kt
+++ app/src/main/java/com/trainingapp/personaltrainingassistant/ui/adapters/AddExerciseSecondaryRVAdapter.kt
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.trainingapp.personaltrainingassistant.R
 import com.trainingapp.personaltrainingassistant.objects.MuscleJoint
 
+/**
+ * Adapter to display exercise secondary movers that the user can select. Multiple movers can be selected so an Array of Boolean values is kept to keep track of which items are selected
+ * @param muscleJoints List of all the possible muscles or joints the user can choose from
+ * @param clickListener Function that handles the onClick event of an item within the AddEditExerciseActivity
+ */
 class AddExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, private val clickListener: (MuscleJoint, Boolean) -> Unit): RecyclerView.Adapter<AddExerciseSecondaryRVAdapter.AddExerciseSecondaryViewHolder>() {
 
-    val isSelected = Array(muscleJoints.size) {false}
+    val isSelected = Array(muscleJoints.size) {false}//running journal of which items are selected
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddExerciseSecondaryViewHolder {
         return AddExerciseSecondaryViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.mover_row, parent, false))
@@ -26,7 +31,7 @@ class AddExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, pr
 
         fun onBindItems(muscleJoint: MuscleJoint, position: Int){
             itemView.findViewById<TextView>(R.id.txtExerciseMoverName).text = muscleJoint.name
-            itemView.setOnClickListener(this)
+            itemView.setOnClickListener(this)//used to internally handle the onClick event and use passed function
             itemView.isSelected = isSelected[position]
         }
 
@@ -34,8 +39,8 @@ class AddExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, pr
             val position = adapterPosition
             isSelected[position] = !isSelected[position]
             itemView.isSelected = !itemView.isSelected
-            clickListener(muscleJoints[position], isSelected[position])
-            notifyItemChanged(position)
+            clickListener(muscleJoints[position], isSelected[position])//sends selected/deselected item to activity to add/remove the secondary mover
+            notifyItemChanged(position)//notify adapter that item state has changed
         }
     }
 }
