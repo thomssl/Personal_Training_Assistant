e6f37f8bce9007162bef6d07a17fe3fbcb3b249d
thomssl <steventhomson72@yahoo.ca>
2020-01-07 09:32:06 -0500

    Documented EditExerciseSecondaryRVAdapter

--- app/src/main/java/com/trainingapp/personaltrainingassistant/ui/adapters/EditExerciseSecondaryRVAdapter.kt
+++ app/src/main/java/com/trainingapp/personaltrainingassistant/ui/adapters/EditExerciseSecondaryRVAdapter.kt
@@ -8,13 +8,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.trainingapp.personaltrainingassistant.R
 import com.trainingapp.personaltrainingassistant.objects.MuscleJoint
 
+/**
+ * Adapter to display the secondary movers that an exercise already contains and can be edited
+ * @param muscleJoints List of all the possible muscles or joints the user can choose from
+ * @param secondaryMovers Movers contained within the Exercise that need to be set as selected upon init
+ * @param clickListener Function that handles the onClick event of an item within the AddEditExerciseActivity
+ */
 class EditExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, private val secondaryMovers: ArrayList<MuscleJoint>, private val clickListener: (MuscleJoint, Boolean) -> Unit): RecyclerView.Adapter<EditExerciseSecondaryRVAdapter.AddExerciseSecondaryViewHolder>() {
 
     val isSelected = Array(muscleJoints.size) {false}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddExerciseSecondaryViewHolder {
-        if (secondaryMovers.isNotEmpty())
-            muscleJoints.forEach { isSelected[muscleJoints.indexOf(it)] = secondaryMovers.contains(it) }
+        if (secondaryMovers.isNotEmpty())//check to make sure the list has items
+            muscleJoints.forEach { isSelected[muscleJoints.indexOf(it)] = secondaryMovers.contains(it) }//for each possible MuscleJoint, check to see if that item should be selected
         return AddExerciseSecondaryViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.mover_row, parent, false))
     }
 
@@ -28,7 +34,7 @@ class EditExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, p
 
         fun onBindItems(muscleJoint: MuscleJoint, position: Int){
             itemView.findViewById<TextView>(R.id.txtExerciseMoverName).text = muscleJoint.name
-            itemView.setOnClickListener(this)
+            itemView.setOnClickListener(this)//used to internally handle the onClick event and use passed function
             itemView.isSelected = isSelected[position]
         }
 
@@ -36,8 +42,8 @@ class EditExerciseSecondaryRVAdapter(val muscleJoints: ArrayList<MuscleJoint>, p
             val position = adapterPosition
             isSelected[position] = !isSelected[position]
             itemView.isSelected = !itemView.isSelected
-            clickListener(muscleJoints[position], isSelected[position])
-            notifyItemChanged(position)
+            clickListener(muscleJoints[position], isSelected[position])//sends selected/deselected item to activity to add/remove the secondary mover
+            notifyItemChanged(position)//notify adapter that item state has changed
         }
     }
 }
