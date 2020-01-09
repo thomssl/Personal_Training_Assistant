f08bc64584ab6d6af3beed7f55084d971a0c6f3c
thomssl <steventhomson72@yahoo.ca>
2020-01-09 13:52:16 -0500

    Documented Exercises UI

--- app/src/main/java/com/trainingapp/personaltrainingassistant/ui/exercises/ExercisesRVAdapter.kt
+++ app/src/main/java/com/trainingapp/personaltrainingassistant/ui/exercises/ExercisesRVAdapter.kt
@@ -1,6 +1,5 @@
 package com.trainingapp.personaltrainingassistant.ui.exercises
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.trainingapp.personaltrainingassistant.objects.Exercise
 import com.trainingapp.personaltrainingassistant.R
 
+/**
+ * Adapter to display all exercises found in the database. Displays all pertinent information about the exercise
+ * @param exercises List of all exercises as Exercise objects
+ * @param clickListener Function used by ExercisesFragment to handle item onClick event (ie edit exercise)
+ * @param longClickListener Function used by ExercisesFragment to handle item onLongClick event (ie delete exercise)
+ */
 class ExercisesRVAdapter(private val exercises: ArrayList<Exercise>, private val clickListener: (Int) -> Unit, private val longClickListener: (Exercise) -> Boolean): RecyclerView.Adapter<ExercisesRVAdapter.ExerciseViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
