8f0d4fc3c8a80eea9a855ae45561dd2fa7875a88
Steven <steventhomson72@yahoo.ca>
2020-06-27 12:50:44 -0400

    Changed structure to make code more readable

--- app/src/main/java/com/trainingapp/trainingassistant/objects/Client.kt
+++ app/src/main/java/com/trainingapp/trainingassistant/objects/Client.kt
@@ -3,10 +3,13 @@ package com.trainingapp.trainingassistant.objects
 /**
  * Object to hold information gathered about a client
  * @param id Unique key given to the client when a new client is inserted. Used to differentiate session data in the database
- * @param name Unique string of text given as the client's name. User submitted but must be unique to make data more readable. User can use any form of differentiation they desire
+ * @param name Unique string of text given as the client's name. User submitted but must be unique to make data more readable.
+ *             User can use any form of differentiation they desire
  * @param schedule Schedule given to client. Used to define how a session is added or tracked for a given client. See Schedule class documentation
- * @param startDate String formatted as yyyy-MM-dd for the start date of the client (for constant or variable scheduled clients) or '0' (for no schedule clients)
- * @param endDate String formatted as yyyy-MM-dd for the end date of the client (for constant or variable scheduled clients) or '0' (for no schedule clients)
+ * @param startDate String formatted as yyyy-MM-dd for the start date of the client (for constant or variable scheduled clients)
+ *                  or '0' (for no schedule clients)
+ * @param endDate String formatted as yyyy-MM-dd for the end date of the client (for constant or variable scheduled clients)
+ *                or '0' (for no schedule clients)
  */
 class Client (var id: Int, var name: String, var schedule: Schedule, var startDate: String, var endDate: String) {
 
@@ -23,7 +26,9 @@ class Client (var id: Int, var name: String, var schedule: Schedule, var startDa
                 "Values('$name', '$startDate', '$endDate',${schedule.getInsertCommand()});"
     }
     fun getUpdateCommand(): String {
-        return "Update Clients Set client_name='$name',start_date='$startDate',end_date='$endDate',${schedule.getUpdateCommand()} Where client_id=$id;"
+        return  "Update Clients " +
+                "Set client_name='$name',start_date='$startDate',end_date='$endDate',${schedule.getUpdateCommand()} " +
+                "Where client_id=$id;"
     }
     fun getDeleteCommand():String {
         return  "Delete From Clients         Where client_id = $id; " +
