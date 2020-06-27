7df890095e2680ad078f29157781d5ea144cfe14
Steven <steventhomson72@yahoo.ca>
2020-06-27 12:49:21 -0400

    Changed structure to make code more readable

--- app/src/main/java/com/trainingapp/trainingassistant/objects/Exercise.kt
+++ app/src/main/java/com/trainingapp/trainingassistant/objects/Exercise.kt
@@ -5,13 +5,19 @@ import com.trainingapp.trainingassistant.enumerators.ExerciseType
 /**
  * Object to hold gathered data about an exercise (abstract exercise definition, not sets, reps or resistance)
  * @param id Unique key given to the exercise when a new exercise is inserted. Used to differentiate exercises within sessions
- * @param name Unique string of text given as the exercise's name. User submitted but must be unique to differentiate exercises. User can use any form of differentiation they desire
+ * @param name Unique string of text given as the exercise's name. User submitted but must be unique to differentiate exercises.
+ *             User can use any form of differentiation they desire
  * @param type ExerciseType enum value given to the exercise. Used to define what the primary and secondary movers are (Muscles or Joints)
  * @param primaryMover MuscleJoint object containing the name and id to the primary mover
  * @param secondaryMovers List of the MuscleJoint objects that contain the names and ids of the secondary movers
  */
-class Exercise(val id: Int, var name: String, var type: ExerciseType, var primaryMover: MuscleJoint, private val secondaryMovers: ArrayList<MuscleJoint>) {
-
+class Exercise(
+    val id: Int,
+    var name: String,
+    var type: ExerciseType,
+    var primaryMover: MuscleJoint,
+    private val secondaryMovers: ArrayList<MuscleJoint>
+) {
     /**
      * Method to get the names of all the secondary movers as a joined string
      */
@@ -48,8 +54,13 @@ class Exercise(val id: Int, var name: String, var type: ExerciseType, var primar
     fun clearSecondaryMovers() = secondaryMovers.clear()
 
     //database operations
-    fun getInsertCommand(): String = "Insert Into Exercises(exercise_name, exercise_type, primary_mover) Values('$name', ${type.num}, ${primaryMover.id});${getDatabaseSecondaryMovers()}"
-    fun getUpdateCommand(): String = "Update Exercises Set exercise_name = '$name', exercise_type = ${type.num}, primary_mover = ${primaryMover.id} Where exercise_id = $id; Delete From Secondary_Movers Where exercise_id = $id;" +
-        getDatabaseSecondaryMovers()
-    fun getDeleteCommand(): String = "Delete From Exercises Where exercise_id = $id;Delete From Secondary_Movers Where exercise_id = $id"
+    fun getInsertCommand(): String = "Insert Into Exercises(exercise_name, exercise_type, primary_mover) " +
+                                     "Values('$name', ${type.num}, ${primaryMover.id});${getDatabaseSecondaryMovers()}"
+    fun getUpdateCommand(): String = "Update Exercises " +
+                                     "Set exercise_name = '$name', exercise_type = ${type.num}, primary_mover = ${primaryMover.id} " +
+                                     "Where exercise_id = $id;" +
+                                     "Delete From Secondary_Movers Where exercise_id = $id;" +
+                                     getDatabaseSecondaryMovers()
+    fun getDeleteCommand(): String = "Delete From Exercises Where exercise_id = $id;" +
+                                     "Delete From Secondary_Movers Where exercise_id = $id"
 }
